fd90fb396c48028de4adb81b6cc3c9663174253a
Shivam <thisisshivam06@gmail.com>
2022-08-26 20:27:10 +0530

    button added

--- src/WebcamCapture.js
+++ src/WebcamCapture.js
@@ -1,5 +1,6 @@
-import React, { useRef } from "react";
+import React, { useCallback, useRef } from "react";
 import Webcam from "react-webcam";
+import RadioButtonUncheckedIcon from "@material-ui/icons/RadioButtonUnchecked";
 
 const videoConstraints = {
   width: 250,
@@ -10,6 +11,11 @@ const videoConstraints = {
 function WebcamCapture() {
   const webcamRef = useRef(null);
 
+  const capture = useCallback(() => {
+    const imageSrc = webcamRef.current.getScreenshot();
+    console.log(imageSrc);
+  }, [webcamRef]);
+
   return (
     <div className="webcamCapture">
       <Webcam
@@ -19,6 +25,12 @@ function WebcamCapture() {
         screenshotFormat="image/jpeg"
         width={videoConstraints.width}
         videoConstraints={videoConstraints}
+      />
+
+      <RadioButtonUncheckedIcon
+        className="webcamCapture__button"
+        onClick={capture}
+        fontSize="large"
       />
     </div>
   );
