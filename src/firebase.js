bad48e8210475075b2df2ae390832806eaef671d
Shivam <thisisshivam06@gmail.com>
2022-08-26 19:02:20 +0530

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBS9Bruo4Dw1iQJutthhw-49-5mVQTme7E",
+  authDomain: "snapchat-clone-yt-25232.firebaseapp.com",
+  projectId: "snapchat-clone-yt-25232",
+  storageBucket: "snapchat-clone-yt-25232.appspot.com",
+  messagingSenderId: "900212656738",
+  appId: "1:900212656738:web:725cf0f0fbec54583b5f3b",
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const storage = firebase.storage();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export { db, auth, storage, provider };
